1b7610a249f34c483e6f3ecd60979c754affd4e3
Andy Adams <andrewdcadams@gmail.com>
2020-07-09 15:15:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,5 @@
 def my_each(array)
-  if block_given?
+
     i = 0
    
     while i < array.length
@@ -7,8 +7,7 @@ def my_each(array)
       i += 1 
     end
     array
-  else
-    puts "Hey! No block was given!"
-  end
+
+
   
 end
